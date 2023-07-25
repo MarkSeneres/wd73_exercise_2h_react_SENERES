c3229470bfb2558e41b4fb26506b4ab92f9ea28e
Mark Dave <markdave.seneres@gamil.com>
2023-07-25 14:44:28 +0800

    Styling a navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
-
+import "./Navbar.css"
 function Navbar() {
   return (
     <>
-    <h2 className="logo">Lo<span>go</span></h2>
+    
       <nav>
+      <h1 className="logo">Lo<span>go</span></h1>
         <ul>
-        <li><Link to="/">Home</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link className="navbar-link" to="/">Home</Link></li>
+          <li><Link className="navbar-link"  to="/products">Products</Link></li>
+          <li><Link className="navbar-link" to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </>
